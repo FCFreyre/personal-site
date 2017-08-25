94a809b6aec4210b3dcf5221e9973da3f988a2c0
F. Coulter Freyre <fcoulterfreyre@gmail.com>
2017-08-25 10:03:12 -0600

    fixed readme, yarn is in gitignore

--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import About from './components/About'
+import Contact from './components/Contact'
+import Header from './components/Header'
+import Projects from './components/Projects'
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route path="/" component={App} />
+      <Route exact path="/" component={About} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/header" component={Header} />
+      <Route exact path="/projects" component={Projects} />
+    </div>
+  </Router>
+  ,
+  document.getElementById('root'));
